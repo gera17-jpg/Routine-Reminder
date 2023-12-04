108aa7abf9733980160ba391bda7ac7799bb4ecd
gera17-jpg <utorrent212@gmail.com>
2023-12-04 19:49:09 +0800

    Modified the code so that the function will be on repeat until a certain condition is met

--- routine.js
+++ routine.js
@@ -1,16 +1,19 @@
-let alarm = 'stopped';
+let alarm = 'rang'; // Assuming the initial state of the alarm is 'rang'
 let volume = 5;
 
-function snoozed() {
-    while (alarm === 'rang') {
+function snooze() {
+    if (alarm === 'snoozed') {
         volume++; // Increase the volume
-        console.log('The volume for the next alarm is Increased');
+        console.log('The volume for the next alarm is increased');
         console.log(volume);
-        
-        // Add a condition to break the loop when desired
-        if (volume >= 10) {
-            alarm = 'stopped'; // Reset alarm status to stop the loop
-            break; // Exit the loop
+        if (alarm !== 'stopped') {
+            snooze(); // Call itself if the alarm is not stopped
         }
     }
 }
+
+// Updated function call on button click
+document.querySelector('button').addEventListener('click', function() {
+    alarm = 'snoozed'; // Set alarm to 'snoozed' when the button is clicked
+    snooze(); // Start the snooze process
+});
