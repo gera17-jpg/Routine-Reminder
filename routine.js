c2226f54f988feacb6660809ed29a76942fb4967
gera17-jpg <utorrent212@gmail.com>
2023-12-05 16:48:22 +0800

    Did some modification but I'm still having trouble stopping the audio after clicking the snooze or stop button

--- routine.js
+++ routine.js
@@ -1,19 +1,48 @@
-let alarm = 'rang'; // Assuming the initial state of the alarm is 'rang'
+let alarmStatus = 'notRinging';
 let volume = 5;
+let snoozeTimeout; // To store the timeout ID
+let audio;
 
-function snooze() {
-    if (alarm === 'snoozed') {
-        volume++; // Increase the volume
-        console.log('The volume for the next alarm is increased');
-        console.log(volume);
-        if (alarm !== 'stopped') {
-            snooze(); // Call itself if the alarm is not stopped
+function startAlarm() {
+    alarmStatus = 'ringing';
+    alarm();
+}
+
+function checkAlarm() {
+    setInterval(() => {
+        const currentTime = new Date().toLocaleTimeString('en-US', { hour12: false });
+        //if (currentTime === '05:30:00') {
+        if (alarmStatus === 'snoozed'){
+            alarm();
         }
+    }, 1000); // Check every second
+}
+
+function alarm() {
+    if (alarmStatus === 'ringing') {
+        audio = new Audio('Why So Lonely.mp3');
+        audio.play();
     }
 }
 
-// Updated function call on button click
-document.querySelector('button').addEventListener('click', function() {
-    alarm = 'snoozed'; // Set alarm to 'snoozed' when the button is clicked
-    snooze(); // Start the snooze process
-});
+function snooze() {
+    let alarmStatus='snoozed'
+
+    if (alarmStatus === 'snoozed') {
+        volume++;
+        console.log('The volume for the next alarm is increased:', volume);
+            // Logic for snooze: Stop the current alarm and set a new one after 5 minutes
+        clearTimeout(snoozeTimeout); // Clear any existing timeout
+        snoozeTimeout = setTimeout(alarm, 5 * 60 * 1000); // Snooze for 5 minutes (5 * 60 * 1000 milliseconds)
+    }
+}
+
+function stop() {
+    if (alarmStatus === 'ringing') {
+        if (audio) {
+            audio.pause();
+        } // Pause the audio playback if it's active
+        console.log("Good morning, it's time to start the day!");
+        alarmStatus = 'stopped';
+        }
+    }
